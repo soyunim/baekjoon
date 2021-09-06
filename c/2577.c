e14ba09bfe1ee8cb3e1efdc006cc6b19106f13d7
soyunim <togksk123@naver.com>
2021-09-06 19:24:51 +0900

    modified

---
+++ c/2577.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    int multi = 1;
+    int result;
+    int arr[10]={0,};
+
+    for(int i=0; i<3; i++){
+        scanf("%d",&num);
+
+        multi *= num;
+    }
+
+    for(int i=0; multi>0; i++){
+        result = multi % 10;
+        arr[result]+=1;
+        multi/=10;
+    }
+
+    for(int i=0; i<10; i++){
+        printf("%d\n", arr[i]);
+    }
+
+}
