fe1443f82a2da00c176ba3ba28aa52abcfc98b2a
soyunim <togksk123@naver.com>
2021-09-08 14:23:43 +0900

    modified

--- c/1546.c
+++ c/1546.c
@@ -4,7 +4,7 @@ int main(){
     int n;
     scanf("%d\n",&n);
     double max=0.0, result=0.0, ave=0.0;
-    double sub[1000];
+    double sub[1000]={0,};
     for(int i=0; i<n; i++){
         scanf("%d ", &sub[i]);
         if(max<sub[i]){
