ed9bfb2ee431ece4cc68391e7aca0380c8676306
soyunim <togksk123@naver.com>
2021-09-28 22:58:08 +0900

    210928

---
+++ c/11654.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    char alphabet;
+    scanf("%c", &alphabet);
+    printf("%d", alphabet);
+}
---
+++ cpp/11654.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char alphabet;
+    cin>>alphabet;
+    cout<<(int)alphabet;
+}
